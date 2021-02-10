dd950e14e139ea1117081a1a071f1a5045adb487
190255 <190255xk@yse-c.net>
2021-02-10 10:09:08 +0900

    first_commit

---
+++ test.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>テスト</title>
+</head>
+<body>
+    
+    <p>PHPのテストです。</p>
+
+    <p>
+    <?php
+        echo '2021年', '2月', '10日', '<br />';
+        print "水曜日";
+        print "<br />";
+        for ($i = 0; $i <= 10; $i++) {
+            echo $i;
+        }
+    ?>
+    </p>
+</body>
+</html>
